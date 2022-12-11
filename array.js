176cf4db76021ad0c63e5a853333af461c13ebcf
henryamos <amoshenry95@gmail.com>
2022-12-11 22:36:31 +0000

    Second commit

--- array.js
+++ array.js
@@ -1,8 +1,11 @@
 
-const number = [1,2,5,8,9,12];
-const nRange = [4,12];
-const lowest = number.find( function(low){
+const number = [7, 23, 3, 1, 3, 5, 2] ;
+const nRange = [2,7];
+const lowest = number.filter(function(low){
     const numbers = low > nRange[0] && low <= nRange[1];
     return numbers;
+    
 });
-console.log(`The lowest numbers is ${lowest}`);
+
+const minimum = Math.min(...lowest);
+console.log(`The lowest number is ${minimum}`);
