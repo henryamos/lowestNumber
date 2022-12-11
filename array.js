223ac85dc8c0b3bfa626480932cf1b691915bafb
henryamos <amoshenry95@gmail.com>
2022-12-11 23:22:06 +0000

    Range value fixed

--- array.js
+++ array.js
@@ -1,11 +1,14 @@
 
-const number = [7, 23, 3, 1, 3, 5, 2] ;
-const nRange = [2,7];
+const number = [1, 3, 2];
+const nRange = [1,1];
 const lowest = number.filter(function(low){
-    const numbers = low > nRange[0] && low <= nRange[1];
+    const numbers = low > nRange[0] && low < nRange[1];
     return numbers;
     
 });
-
-const minimum = Math.min(...lowest);
+if(lowest.length == 0){
+    console.log(0);
+}else{
+    const minimum = Math.min(...lowest);
 console.log(`The lowest number is ${minimum}`);
+}
